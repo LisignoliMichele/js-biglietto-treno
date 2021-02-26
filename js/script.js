03adf8751e9331a567b4abe1c0b06f49e563db76
Michele Lisignoli <michele@lisignoli.com>
2021-02-26 17:03:50 +0100

    PLEASE ENTER ONLY NUMBERS

--- js/script.js
+++ js/script.js
@@ -2,25 +2,31 @@
 var kmCost = 0.21;
 
 // how many km
-var howManyKm = prompt("how Many Km?");
-
+var howManyKm = parseInt(prompt("How many km will you travel to get your destination?"));
+if (isNaN(howManyKm)) {
+  howManyKm = prompt("How many km will you travel to get your destination? PLEASE ENTER ONLY NUMBERS");
+}
 //normal price
 var normalPrice = kmCost * howManyKm;
-
+if (isNaN(age)) {
+  age = prompt("how old are you? PLEASE ENTER ONLY NUMBERS");
+}
 // age
-var age = prompt("how old are you?")
+var age = parseInt(prompt("how old are you?"));
 
 // discount for young 20%
 var youngDiscont = normalPrice * 0.8;
+var youngSaved = normalPrice - youngDiscont;
 
 //old discount 40%
 var oldDiscount = normalPrice * 0.6;
+var oldSaved = normalPrice - oldDiscount;
 
 // result
 if (age < 18) {
-  alert( "the price for your ticket is " + youngDiscont + "€" )
+  alert( "the price for your ticket is " + youngDiscont + "€. " + "You're saving " + youngSaved + "€ on the full price." )
 }else if (age >= 65) {
-  alert( "the price for your ticket is " +  oldDiscount + "€" )
+  alert( "the price for your ticket is " +  oldDiscount + "€. " + "You're saving " + oldSaved + "€ on the full price." )
 }else{
-  alert( "the price for your ticket is " +  normalPrice + "€" )
+  alert( "the price for your ticket is " +  normalPrice + "€." )
 }
